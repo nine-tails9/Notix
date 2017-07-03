ce9c3e29a56da82efb3b16cd14f526d8cf62c05b
Deepanshu Gandhi <Deepanshu Gandhi>
2017-07-03 20:08:39 +0530

    View page Included

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -31,14 +31,56 @@
     </div>
 
     </nav>
+    <!-- End of Nav Bar -->
+    <div class="tile is-ancestor notification is-dark is-medium">
+
+        <h3 class="title has-text-centered is-dark tile is-parent is-bold">Add A Note</h3>
+
+        <div class="tm-body">
+
+        <form class="" action="index.html" method="post">
+          <div class="field">
+            <label class="label">Author</label>
+            <p class="control has-icons-left">
+              <input v-model="Note.author" class="input is-success" type="text" placeholder="Your Name">
+              <span class="icon is-small is-left">
+                <i class="fa fa-user"></i>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">Title</label>
+            <p class="control">
+              <input type="text" v-model="Note.title" placeholder="Title" value="" class="input is-warning">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control"><label class="label">Content</label>
+            <textarea class="textarea" v-model="Note.content" placeholder="Start Typing!"></textarea>
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <a class="button is-light is-pulled-left">Save</a>
+            </p>
+
+          </div>
+        </form>
+
+        </div>
+
 
   </div>
+
+
+</div>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      Note : {author : '',title: '', content: ''}
 
     }
   }
@@ -46,5 +88,22 @@ export default {
 </script>
 
 <style>
+h3{
+  margin-top: 30px;
+}
 
+.hero-body{
+  margin-left: 50px;
+}
+.tm-body{
+  width: 600px;
+  margin-left: 70px;
+  margin-top: 10%;
+}
+.tm-body .field{
+  width: 600px;
+}
+.label{
+  color: #fff;
+}
 </style>
