b2c48c0a9795745080922ba5bc78e7c122d5d7fc
Deepanshu Gandhi <Deepanshu Gandhi>
2017-07-03 19:09:08 +0530

    Completed Nav Bar

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <note-view></note-view>
+
+  </div>
+</template>
+
+<script>
+import NoteV from './components/Create.vue';
+export default {
+  components: {
+    'note-view' : NoteV
+  },
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Create.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nav class="nav has-shadow">
+      <div class="nav-left">
+        <a class="nav-item">
+          <i class="fa fa-sticky-note fa-fw fa-5x"></i> &nbsp;<h2 class="title is-4">Notix</h2>
+        </a>
+      </div>
+      <div class="nav-center">
+    <a class="nav-item">
+      <span class="icon">
+        <i class="fa fa-github"></i>
+      </span>
+    </a>
+    <a class="nav-item">
+      <span class="icon">
+        <i class="fa fa-twitter"></i>
+      </span>
+    </a>
+  </div>
+    <div class="nav-right nav-menu">
+      <a href="#" class="nav-item is-tab">Create</a>
+      <a href="#" class="nav-item is-tab">View</a>
+      <a href="#" class="nav-item is-tab">About Us!</a>
+      <div class="nav-item">
+        <a href="#" class="button is-outlined is-info"><div class="icon"><i class="fa fa-facebook"></i></div>
+          &nbsp;&nbsp;Share on Facebook
+        </a>
+
+      </div>
+    </div>
+
+    </nav>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
